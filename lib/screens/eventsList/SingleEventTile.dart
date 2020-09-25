73d5f1987b3a9eb77dae466664a4ded6792b7a2f
Łukasz Goleniec <30418440+Flashrow@users.noreply.github.com>
2020-09-25 10:39:32 +0200

    Update SingleEventTile.dart

--- lib/screens/eventsList/SingleEventTile.dart
+++ lib/screens/eventsList/SingleEventTile.dart
@@ -10,24 +10,29 @@ class SingleEventTile extends StatelessWidget {
       padding: const EdgeInsets.fromLTRB(18, 0, 18, 0),
       child: Stack(
         children: [
-          Hero(
-            tag: 'Image',
-            child: AspectRatio(
-              aspectRatio: 2 / 1,
-              child: Container(
-                child: ClipRRect(
-                  child: Image(
-                    image: AssetImage('assets/images/lake.jpg'),
-                    height: 260,
-                    fit: BoxFit.cover,
-                  ),
-                  borderRadius: BorderRadius.circular(14),
+          AspectRatio(
+            aspectRatio: 2 / 1,
+            child: Container(
+              child: ClipRRect(
+                child: Image(
+                  image: AssetImage('assets/images/lake.jpg'),
+                  height: 260,
+                  fit: BoxFit.cover,
                 ),
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(14),
-                    boxShadow: [Shadow.get()]),
+                borderRadius: BorderRadius.circular(14),
               ),
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(14),
+                  boxShadow: [Shadow.get()]),
             ),
+          ),
+          Container(
+            height: 55,
+            width: 40,
+            decoration: BoxDecoration(
+              color: Colors.white.withOpacity(0.85),
+            ),
+            transform: Matrix4.translationValues(8, 8, 0),
           ),
         ],
       ),
