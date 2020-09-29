e5e811e51ee964de928c95116769ba7ff5f90847
Kamil <kamilpier08@gmail.com>
2020-09-29 14:22:24 +0200

    icons

--- lib/screens/eventWrapper/createEventPlace/CreateEventPlace.dart
+++ lib/screens/eventWrapper/createEventPlace/CreateEventPlace.dart
@@ -1,7 +1,8 @@
 import 'package:conmi/utils/Colors.dart';
-import 'package:conmi/widgets/ConmiFontStyle.dart';
+import 'package:conmi/utils/EventIcons.dart';
 import 'package:conmi/widgets/RectGradientTopBar.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 class CreateEventPlace extends StatelessWidget {
   const CreateEventPlace({Key key}) : super(key: key);
@@ -32,150 +33,168 @@ class CreateEventPlaceBody extends StatefulWidget {
 }
 
 class _CreateEventPlaceBodyState extends State<CreateEventPlaceBody> {
+  String gradient = 'primary';
+  String icon = 'beach_access';
+
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      physics: NeverScrollableScrollPhysics(),
-      child: ConstrainedBox(
-        constraints: BoxConstraints(
-          minWidth: MediaQuery.of(context).size.width,
-          minHeight: MediaQuery.of(context).size.height,
-        ),
-        child: IntrinsicHeight(
-          child: Column(
-            children: [
-              Expanded(
-                flex: 3,
-                child: Container(
-                  alignment: Alignment.topCenter,
-                  padding: EdgeInsets.only(top: 50),
-                  child: FittedBox(
-                    child: Container(
-                      decoration: BoxDecoration(
-                        gradient: LinearGradient(
-                          colors: [ConmiColor().primary, ConmiColor().secondary],
-                          begin: Alignment.bottomLeft,
-                          end: Alignment.topRight,
-                        ),
-                        borderRadius: BorderRadius.circular(14.0),
-                      ),
-                      child: Column(
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(40, 50, 40, 10),
-                            child: Center(
-                              child: Container(
-                                width: 110,
-                                height: 110,
-                                decoration: BoxDecoration(
-                                  shape: BoxShape.circle,
-                                  border: Border.all(
-                                    width: 5,
-                                    color: Colors.white30,
-                                  ),
-                                ),
-                                child: Icon(
-                                  Icons.beach_access,
-                                  size: 70,
-                                  color: Colors.white,
-                                ),
-                              ),
+    return Column(
+      children: [
+        Expanded(
+          child: Container(
+            alignment: Alignment.topCenter,
+            padding: EdgeInsets.only(top: 50),
+            child: FittedBox(
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: ConmiColor().gradients[gradient],
+                    begin: Alignment.bottomLeft,
+                    end: Alignment.topRight,
+                  ),
+                  borderRadius: BorderRadius.circular(14.0),
+                ),
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.fromLTRB(40, 50, 40, 10),
+                      child: Center(
+                        child: Container(
+                          width: 110,
+                          height: 110,
+                          decoration: BoxDecoration(
+                            shape: BoxShape.circle,
+                            border: Border.all(
+                              width: 5,
+                              color: Colors.white30,
                             ),
                           ),
-                          Container(
-                            width: 160,
-                            padding: EdgeInsets.only(bottom: 30),
-                            child: TextField(
-                              textAlign: TextAlign.center,
-                              decoration: InputDecoration(
-                                hintText: "Nazwa",
-                                hintStyle: TextStyle(
-                                  color: Colors.white70,
-                                  fontSize: 20,
-                                  fontWeight: FontWeight.w500,
-                                ),
-                                enabledBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white38),
-                                ),
-                                focusedBorder: UnderlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white38),
-                                ),
-                                border: UnderlineInputBorder(
-                                  borderSide: BorderSide(color: Colors.white38),
-                                ),
-                              ),
-                              style: TextStyle(
-                                color: Colors.white,
-                                fontSize: 20,
-                                fontWeight: FontWeight.w500,
-                              ),
-                              textCapitalization: TextCapitalization.characters,
-                            ),
-                          )
-                        ],
+                          child: Icon(
+                            eventIcons[icon],
+                            size: 70,
+                            color: Colors.white,
+                          ),
+                        ),
                       ),
                     ),
-                  ),
+                    Container(
+                      width: 160,
+                      padding: EdgeInsets.only(bottom: 30),
+                      child: TextField(
+                        textAlign: TextAlign.center,
+                        decoration: InputDecoration(
+                          hintText: "Nazwa",
+                          hintStyle: TextStyle(
+                            color: Colors.white70,
+                            fontSize: 20,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white38),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white38),
+                          ),
+                          border: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Colors.white38),
+                          ),
+                        ),
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500,
+                        ),
+                        textCapitalization: TextCapitalization.characters,
+                      ),
+                    )
+                  ],
                 ),
               ),
-              Expanded(
-                flex: 4,
-                child: Container(
-                  width: double.infinity,
-                  height: MediaQuery.of(context).size.height / 2,
-                  padding: EdgeInsets.symmetric(horizontal: 40),
+            ),
+          ),
+        ),
+        Expanded(
+          child: Container(
+            padding: EdgeInsets.symmetric(horizontal: 40),
+            child: Column(
+              children: [
+                SizedBox(
+                  height: 60,
+                  child: ListView(
+                    scrollDirection: Axis.horizontal,
+                    children: ConmiColor().gradients.entries.map((entry) => buildGradientButton(entry)).toList(),
+                  ),
+                ),
+                Expanded(
                   child: GridView.count(
                     crossAxisCount: 4,
                     crossAxisSpacing: 15,
-                    children: [
-                      buildGradientButton(),
-                      buildGradientButton(),
-                      buildGradientButton(),
-                      buildGradientButton(),
-                      buildIconButton(icon: Icons.account_balance_wallet),
-                      buildIconButton(icon: Icons.account_balance_wallet),
-                      buildIconButton(icon: Icons.account_balance_wallet),
-                      buildIconButton(icon: Icons.account_balance_wallet),
-                      buildIconButton(icon: Icons.account_balance_wallet),
-                      buildIconButton(icon: Icons.account_balance_wallet),
-                      buildIconButton(icon: Icons.account_balance_wallet),
-                      buildIconButton(icon: Icons.account_balance_wallet),
-                    ],
+                    children: eventIcons.entries.map((entry) => buildIconButton(entry)).toList(),
                   ),
                 ),
-              ),
-            ],
+              ],
+              mainAxisSize: MainAxisSize.min,
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+
+  void setGradient(String gradient) {
+    setState(() {
+      this.gradient = gradient;
+    });
+  }
+
+  Widget buildGradientButton(entry) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 15),
+      child: GestureDetector(
+        onTap: () => setGradient(entry.key),
+        child: Container(
+          height: 60,
+          width: 60,
+          alignment: Alignment.center,
+          child: AnimatedOpacity(
+            opacity: entry.key == gradient ? 1.0 : 0.0,
+            duration: Duration(milliseconds: 200),
+            child: Icon(
+              Icons.check_circle_outline,
+              color: Colors.white,
+            ),
+          ),
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              colors: entry.value,
+              begin: Alignment.bottomLeft,
+              end: Alignment.topRight,
+            ),
+            borderRadius: BorderRadius.circular(15.0),
           ),
         ),
       ),
     );
   }
 
-  Widget buildGradientButton() {
-    return Container(
-      height: 60,
-      width: 60,
-      child: null,
-      decoration: BoxDecoration(
-        gradient: LinearGradient(
-          colors: [ConmiColor().primary, ConmiColor().secondary],
-          begin: Alignment.bottomLeft,
-          end: Alignment.topRight,
-        ),
-        borderRadius: BorderRadius.circular(15.0),
-      ),
-    );
+  void setIcon(String icon) {
+    setState(() {
+      this.icon = icon;
+    });
   }
 
-  Widget buildIconButton({IconData icon}) {
-    return Container(
-      height: 60,
-      width: 60,
-      alignment: Alignment.center,
-      child: Icon(
-        icon,
-        size: 35.0,
-        color: Colors.black,
+  Widget buildIconButton(entry) {
+    return GestureDetector(
+      onTap: () => setIcon(entry.key),
+      child: Container(
+        height: 60,
+        width: 60,
+        alignment: Alignment.center,
+        child: Icon(
+          entry.value,
+          size: 35.0,
+          color: entry.key == icon ? ConmiColor().primary : Colors.black87,
+        ),
       ),
     );
   }
